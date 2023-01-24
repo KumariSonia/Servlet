5d98b475abe89b005114e35ef6a9df170540ca34
milan bamaniya <milan@fliptree.com>
2023-01-24 11:22:53 +0530

    sending data using cookie

--- src/main/java/com/sonia/web/AddServlet.java
+++ src/main/java/com/sonia/web/AddServlet.java
@@ -3,6 +3,7 @@ package com.sonia.web;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,8 +17,11 @@ public class AddServlet extends HttpServlet {
 		int j = Integer.parseInt(req.getParameter("num1"));
 		int k = i + j;
 
-		HttpSession session = req.getSession();
-		session.setAttribute("k", k); // sending data using session
+//		HttpSession session = req.getSession();
+//		session.setAttribute("k", k); // sending data using session
+
+		Cookie cookie = new Cookie("k", k + ""); // sending data using Cookie
+		resp.addCookie(cookie);
 
 		resp.sendRedirect("sq");
 
--- src/main/java/com/sonia/web/SquareServlet.java
+++ src/main/java/com/sonia/web/SquareServlet.java
@@ -4,17 +4,23 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 public class SquareServlet extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		HttpSession session = req.getSession();
-//session.removeAttribute("k"); //for removing data from session
-		int k = (Integer) session.getAttribute("k");
+		int k = 0;
+
+		Cookie cookies[] = req.getCookies();
+
+		for (Cookie cookie : cookies) {
+			if (cookie.getName().equals("k"))
+				k = Integer.parseInt(cookie.getValue());
+		}
+
 		k = k * k;
 		PrintWriter out = resp.getWriter();
 		out.print("Square of given number = " + k);
