cfc8707b363d6b4b735e6a317c0a693aeee8002f
milan bamaniya <milan@fliptree.com>
2023-01-24 11:51:45 +0530

    Added changes for annotation

--- src/main/java/com/sonia/web/AddServlet.java
+++ src/main/java/com/sonia/web/AddServlet.java
@@ -3,12 +3,13 @@ package com.sonia.web;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
+@WebServlet("/add")
 public class AddServlet extends HttpServlet {
 
 	@Override
